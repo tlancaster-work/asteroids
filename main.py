1dcbb9a6eb3efe485cf4dc3ca9e7c3dde71435ec
tlancaster-work <tylerlancaster.work@gmail.com>
2026-05-01 12:06:06 -0700

    ship rotate and move

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,13 +9,22 @@ def main():
 Screen height: {SCREEN_HEIGHT}""")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pclock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen) # after filling screen, but before flipping the screen
         pygame.display.flip()
+        #pclock.tick(60)
+        # dt always at the end
+        dt = pclock.tick(60) / 1000 # tick returns time passed since last time it was called - delta time. divide to convert from ms to seconds
+
 
 
 if __name__ == "__main__":
