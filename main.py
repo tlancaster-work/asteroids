21acffbfdaec1c8ccce92a3e90321158e4c3fb9e
tlancaster-work <tylerlancaster.work@gmail.com>
2026-05-01 12:32:31 -0700

    asteroids and asteroidfield beginning

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 from logger import log_state
 from player import *
+from asteroid import *
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -10,18 +12,25 @@ Screen height: {SCREEN_HEIGHT}""")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pclock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Player.containers = (updatable, drawable)
     dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    game_asteroidfield = AsteroidField()
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen) # after filling screen, but before flipping the screen
+        for thing in drawable:
+            thing.draw(screen) # after filling screen, but before flipping the screen
         pygame.display.flip()
-        #pclock.tick(60)
         # dt always at the end
         dt = pclock.tick(60) / 1000 # tick returns time passed since last time it was called - delta time. divide to convert from ms to seconds
 
